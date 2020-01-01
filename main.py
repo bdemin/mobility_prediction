20543661f5ddd1c19e0ef9620794254f2973689e
bdemin <42113340+bdemin@users.noreply.github.com>
2020-01-01 17:43:56 +0100

    Remove head() calls

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import pandas as pd
 
-# Read data
-train_df = pd.read_csv('mobility_fake_dataset.csv')
-#check data has been read in properly
-train_df.head()
+    # Read data
+    train_df = pd.read_csv('mobility_fake_dataset.csv')
 
-#create a dataframe with all training data except the target column
-train_X = train_df.drop(columns=['mobility'])
+    #create a dataframe with all training data except the target column
+    train_X = train_df.drop(columns=['mobility'])
 
-#check that the target variable has been removed
-train_X.head()
 
-#create a dataframe with only the target column
-train_y = train_df[['mobility']]
+    #create a dataframe with only the target column
+    train_y = train_df[['mobility']]
 
-#view dataframe
-train_y.head()
 
 
 from keras.models import Sequential
